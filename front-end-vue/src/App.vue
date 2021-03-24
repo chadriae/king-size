56ed074f1cb1de3b68260d2112cd87b2c5fa4a07
Manten Devillé <manten.d@hotmail.com>
2021-03-24 15:36:31 +0100

    Add assets and restile Navbar, add bg to app for clarity

--- front-end-vue/src/App.vue
+++ front-end-vue/src/App.vue
@@ -16,13 +16,21 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+  background-image: url("./assets/bike-bg.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
 #app {
+
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   justify-content: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
--- front-end-vue/src/components/Navbar.vue
+++ front-end-vue/src/components/Navbar.vue
@@ -1,19 +1,84 @@
 <template>
-    <div id='nav'>
-      <router-link to='/'> Home </router-link>
-      <router-link to='/about'>About </router-link>
-      <router-link to='/profiles'>Profiles </router-link>
+  <div id="nav">
+    <div class="navLogo"><img src="../assets/chainswhite.png" alt="" /></div>
+    <div id="navLinks">
+      <ul>
+        <li>
+          <router-link class="routerLi" to="/"> Home </router-link>
+        </li>
+        <li>
+          <router-link class="routerLi" to="/about">About </router-link>
+        </li>
+        <li>
+          <router-link class="routerLi" to="/profiles">Profiles </router-link>
+        </li>
+      </ul>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Navbar'
-}
+  name: "Navbar",
+};
 </script>
 
 <style scoped>
 #nav {
-    background-color: black;
+  background-image: linear-gradient(to top, rgba(255, 0, 0, 0), rgb(0, 0, 0));
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  padding: 0 8vw;
+  height: 10vh;
+  border: 1px solid blue;
+  align-content: center;
 }
+.navLogo {
+  height: 8;
+  width: 8vh;
+  border: 1px solid red;
+  margin: 1vh 0;
+  object-fit: fill;
+  padding-right: 0.5vw;
+  border-right: 0.5vw solid white;
+}
+.navLogo img {
+  height: 100%;
+  width: 100%;
+}
+#navLinks {
+  display: flex;
+  flex-direction: row;
+  border: 1px solid green;
+  margin: 1vh;
+  align-content: center;
+  width: 80vw;
+  justify-content: flex-end;
+}
+ul {
+  display: flex;
+  justify-content: center;
+  padding: 0;
+}
+li {
+  display: block;
+  padding: 0 2vw;
+  list-style-type: none;
+  width: 6vw;
+}
+.navLink {
+  margin: 6vh 2vw;
+  border: 1px solid red;
+  width: 6vw;
+}
+.routerLi {
+  color: #fff;
+  text-decoration: none;
+  text-transform: uppercase;
+  border-bottom: 3px solid transparent;
+}
+.routerLi:hover {
+  font-size: 13pt;
+color: #a6bb98;}
 </style>
