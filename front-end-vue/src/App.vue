f8f56da3f431eb2bce0c8745e391b89c3b7f3f02
Christophe Adriaensens <christophe.adriaensens@gmail.com>
2021-03-24 10:56:14 +0100

    Set up vue router and first pages

--- front-end-vue/src/App.vue
+++ front-end-vue/src/App.vue
@@ -1,15 +1,20 @@
 <template>
+  <div id='root'>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="I am a template"/>
+    <div id='nav'>
+      <router-link to='/'> Home </router-link>
+      <router-link to='/about'>About </router-link>
+      <router-link to='/profiles'>Profiles </router-link>
+    </div>
+    <router-view />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
