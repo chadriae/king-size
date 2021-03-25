ddb430348fe2f2dc35cc950114881bba52c9c86e
Christophe Adriaensens <christophe.adriaensens@gmail.com>
2021-03-25 11:28:33 +0100

    Clean up file structure

--- storage/framework/views/452cc7ad8927ab171c3c0765c531dd7277334151.php
+++
@@ -1,4 +0,0 @@
-<div class="footer">
-    <h4>KingSize</h4>
-    <p>Christophe Adriaensens - Manten Devillé</p>
-</div><?php /**PATH C:\Users\Manten\Desktop\becode\Exercises\04.SP\king-size\resources\views/includes/footer.blade.php ENDPATH**/ ?>
--- storage/framework/views/9b1fc8aacac374c6e26429cd7489a964248db0d2.php
+++
@@ -1,5 +0,0 @@
-
-<?php $__env->startSection('content'); ?>
-    <h1>hello world</h1>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layouts.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Manten\Desktop\becode\Exercises\04.SP\king-size\resources\views/pages/index.blade.php ENDPATH**/ ?>
