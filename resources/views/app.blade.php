e01a1c6f2aa45125a513403d476038cabbf48b5d
Manten Devillé <manten.d@hotmail.com>
2021-03-29 14:11:35 +0200

    Remove cdn from App.blade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,7 +8,6 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
 
         <style>
             body {
