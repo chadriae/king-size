a98dafbae99ff89c3df751f9ff3b0906b2441106
Manten Devillé <manten.d@hotmail.com>
2021-03-29 14:12:50 +0200

    Import animate.css cdn in app.vue

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -22,6 +22,7 @@ export default {
 </script>
 
 <style>
+@import "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css";
 @font-face {
   font-family: "Montserrat";
   src: local("Montserrat"),
