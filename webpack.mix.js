302ef14d870d38fdc7c2adefd024fb0a3637acbb
Christophe Adriaensens <christophe.adriaensens@gmail.com>
2021-04-06 17:55:12 +0200

    Heroku update

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,4 @@
 const mix = require('laravel-mix');
-// const webpack = require('webpack');
 
 /*
  |--------------------------------------------------------------------------
@@ -7,23 +6,11 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').vue()
-.postCss('resources/css/app.css', 'public/css', [
-    require('postcss-import'),
-    require('tailwindcss')('./tailwind.config.js'),
-    require('autoprefixer'),
-]);
-
-// mix.webpackConfig ({
-//     plugins: [
-//       new webpack.DefinePlugin({
-//         __VUE_OPTIONS_API__: false,
-//         __VUE_PROD_DEVTOOLS__: false,
-//       }),
-//     ],
-//   })
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .sourceMaps();
