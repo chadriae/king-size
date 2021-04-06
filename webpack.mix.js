eb242b0d641f7c27e60b85b38491b7cc80bd03cd
Christophe Adriaensens <christophe.adriaensens@gmail.com>
2021-04-06 23:16:36 +0200

    Heroku headaches

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,8 +12,5 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .vue({ version: 3 })
-    .sass('resources/sass/app.scss', 'public/css')
-    .postCss('resources/css/app.css', 'public/css', [
-        require('tailwindcss'),
-    ]);
+    .vue()
+    .sass('resources/sass/app.scss', 'public/css');
